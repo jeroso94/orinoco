e9a7f84ce15a30d83d9f75e0cf390f2b7758a3b8
jeroso94 <jeroso94@gmail.com>
2020-09-01 19:18:04 +0200

    Prise en charge du coloris et de la quantité

--- orinoco-frontend/src/events.js
+++ orinoco-frontend/src/events.js
@@ -10,19 +10,50 @@ class ProductLine {
 export function keepTeddy() {
     const btnToAddInBasket = document.getElementById('toAddInBasket');
     btnToAddInBasket.addEventListener('click', updateBasket);
-    function updateBasket() {
-        let basketReserved = JSON.parse(localStorage.getItem("basketStored")) || [];
-        basketReserved.push(localStorage.selectedProductId);
-        localStorage.setItem("basketStored", JSON.stringify(basketReserved));
 
+    function updateBasket() {
         const newProductLine = new ProductLine (
             localStorage.getItem('selectedProductId'),
             localStorage.getItem('selectedProductColor'),
             localStorage.getItem('selectedProductQty'),
         );
-        console.log(newProductLine.productid, newProductLine.productcolor, newProductLine.productqty);
-        //document.getElementById('toOrder').disabled = false;
-    }    
+        let basketReserved = JSON.parse(localStorage.getItem("basketStored")) || [];
+        // A condition que le panier ne soit pas vide
+        if (JSON.stringify(basketReserved) != '[]') {
+            let sameIdSameColor = null;
+            // Pour chaque ligne du panier
+            for (const elt in basketReserved) {
+                // A condition que le teddy selectionné ait la même référence et le même coloris
+                if ((basketReserved[elt].productid === newProductLine.productid) && (basketReserved[elt].productcolor === newProductLine.productcolor)) {
+                    //Mise à jour théorique des quantités
+                    basketReserved[elt].productqty = parseInt (basketReserved[elt].productqty) + parseInt (newProductLine.productqty);
+                    sameIdSameColor = true;
+                // Autrement, à condition d'avoir lu tout le panier et que le teddy selectionné ait la même référence mais pas le même coloris
+                } else if ((sameIdSameColor === null) && (basketReserved[elt].productid === newProductLine.productid) && (basketReserved[elt].productcolor != newProductLine.productcolor)) {
+                    sameIdSameColor = false;
+                }
+            }
+
+            // En définitif, mise à jour du panier
+            if (sameIdSameColor) {
+                // Pérénisé la nouvelle quantité
+                localStorage.setItem("basketStored", JSON.stringify(basketReserved));
+            } else if (!sameIdSameColor) {
+                // Nouvelle ligne dans le panier & sauvegarde
+                basketReserved.push(newProductLine);
+                localStorage.setItem("basketStored", JSON.stringify(basketReserved));
+            } else {
+                // Nouvelle ligne dans le panier & sauvegarde
+                basketReserved.push(newProductLine);
+                localStorage.setItem("basketStored", JSON.stringify(basketReserved));
+            }
+
+        } else {
+            // Nouvelle ligne dans le panier & sauvegarde
+            basketReserved.push(newProductLine);
+            localStorage.setItem("basketStored", JSON.stringify(basketReserved));
+        }
+    }
 }
 
 export function fillInOrder() {
--- orinoco-frontend/src/order.js
+++ orinoco-frontend/src/order.js
@@ -3,36 +3,37 @@ import euroConverter from './utils';
 // FONCTION GLOBALE - Afficher les oursons ajoutés au panier de commande
 export default function showTeddiesInBasket() {
     let basket = JSON.parse(localStorage.getItem("basketStored"));
+    let midAmount = 0;
     let totalAmount = 0;
     //let totalAmount = parseInt(localStorage.getItem("totalAmountStored")) || 0;
     let basketRow = 0;
 
     for (const item of basket) {
-        console.log (item);
+        console.log (item.productid);
         /* REQUETE API GET pour afficher le détail du produit sélectionné
         -- DEBUT -- */
 
-        const url = 'http://localhost:3000/api/teddies/' + item;
-        console.log(url);
+        const url = 'http://localhost:3000/api/teddies/' + item.productid;
+        //console.log(url);
         fetch(url)
         .then(response => response.json())
-        .then(productsInBasket => {
+        .then(thisItem => {
+            midAmount = parseInt(thisItem.price) * item.productqty;
             let orderResult = document.getElementById('orderResult');
             orderResult.innerHTML += `<tr class='product'>
-                <td>(<a id='${productsInBasket._id}_btnToDelete' href='../views/order.html'>Supprimer</a>)</td>
-                <td>(<a id='${productsInBasket.name}' href='/views/customize.html'>Détails...</a>)</td>
-                <td>${productsInBasket._id}</td>
-                <td><img src='${productsInBasket.imageUrl}' width='50%'/></td>
-                <td>${productsInBasket.name}</td>
-                <td>${'vide'}</td>
-                <td>${'vide'}</td>
-                <td>${euroConverter(productsInBasket.price)}</td>
-                <td>${'vide'}</td>
+                <td>(<a id='${thisItem._id}_btnToDeleteThisLine' href='../views/order.html'>Supprimer</a>)</td>
+                <td>${thisItem._id}</td>
+                <td><img src='${thisItem.imageUrl}' width='50%'/></td>
+                <td><a id='${thisItem.name}' href='/views/customize.html'>${thisItem.name}</a></td>
+                <td id='${item.productcolor}'>${item.productcolor}</td>
+                <td>${item.productqty}</td>
+                <td>${euroConverter(thisItem.price)}</td>
+                <td>${euroConverter(midAmount)}</td>
             </tr>`;
             
-            //console.log (productsInBasket.name + ":" + euroConverter(productsInBasket.price));
+            console.log (orderResult);
 
-            totalAmount += parseInt(productsInBasket.price);
+            totalAmount += midAmount;
             //console.log ("Montant total: " + euroConverter(totalAmount));
             localStorage.setItem("totalAmountStored", totalAmount);
             basketRow ++;
@@ -42,39 +43,33 @@ export default function showTeddiesInBasket() {
                 let totalAmountArea = document.getElementById('totalAmountArea');
                 totalAmountArea.innerHTML += "<h2>Montant total : " + euroConverter(localStorage.getItem("totalAmountStored")) + "</h2>";
             }
+
+            return thisItem;
+        })
+        .then(thisItem => {
+            console.log(thisItem._id + ' ' + thisItem.name);
+            document.getElementById(thisItem.name).onclick = () => {
+                localStorage.setItem ('selectedProductId', thisItem._id);
+                //alert ("LocalStorage : " + localStorage.getItem ('selectedProductId'));
+                location.href = "/views/customize.html";
+            }
+
+            document.getElementById(thisItem._id + '_btnToDeleteThisLine').onclick = (evt) => {
+                evt.preventDefault;
+                let newBasket = [];
+                const searchArray = JSON.parse(localStorage.getItem("basketStored"));
+                for (const elt of searchArray) {
+                    if(elt.productid != thisItem._id) newBasket.push(elt);
+                    if((elt.productid === thisItem._id) && (elt.productcolor != item.productcolor)) newBasket.push(elt);
+                }
+                localStorage.setItem('basketStored', JSON.stringify(newBasket));
+            }
+
         })
         .catch(err => {
             console.log("Quelque chose s'est mal passé durant le GET !", err);
         });
         /* REQUETE API GET pour afficher le détail de l'article sélectionné
-        -- FIN -- */
-
-        /* REQUETE API GET pour afficher le détail de l'article sélectionné depuis le panier
-        -- FIN -- */
-        fetch(url)
-        .then(response => response.json())
-        .then(pIB => {
-            console.log(document.getElementById(pIB.name));
-            document.getElementById(pIB.name).onclick = () => {
-                localStorage.setItem ('selectedProductId', pIB._id);
-                //alert ("LocalStorage : " + localStorage.getItem ('selectedProductId'));
-                location.href = "/views/customize.html";
-            }
-
-            document.getElementById(pIB._id+'_btnToDelete').onclick = (evt) => {
-                evt.preventDefault;
-                let newBasket = [];
-                const array=JSON.parse(localStorage.getItem("basketStored"));
-                for (let token in array) {
-                    if(array[token] != pIB._id) newBasket.push(array[token]);
-                }
-                localStorage.setItem('basketStored', JSON.stringify(newBasket));
-            }
-        })
-        .catch((err) => {
-            console.log("Quelque chose s'est mal passé durant le GET !", err);
-        });
-                /* REQUETE API GET pour afficher le détail de l'article sélectionné depuis le panier
         -- FIN -- */
     }
 }
